2b4316a4e6004d0a038faba3f061e28ddcced9db
prabhakar <prabhakarn@xbi4.com>
2024-01-02 17:19:08 +0530

    Add style in card components

--- src/Component/NewsCard.jsx
+++ src/Component/NewsCard.jsx
@@ -4,11 +4,17 @@ import { Link } from "react-router-dom";
 const NewsCard = ({ title, imageUrl, description, buttonText, linkTo }) => {
   return (
     <div>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img className="w-full" src={imageUrl} alt={title} />
+      <div className="max-w-sm rounded overflow-hidden shadow-lg h-full">
+        <img
+          className="w-full h-56 object-contain"
+          src={imageUrl}
+          alt={title}
+        />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{title}</div>
-          <p className="text-gray-700 text-base">{description}</p>
+          <div className="font-bold text-xl mb-2">{title?.slice(0, 50)}...</div>
+          <p className="text-gray-700 text-base">
+            {description?.slice(0, 100)}...
+          </p>
         </div>
         <div className="px-6 py-4">
           <Link to={linkTo}>
